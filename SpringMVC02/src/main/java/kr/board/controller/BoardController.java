578ec4edbbf6ff70ad25d4ebb8c028cb7067b0ae
egg <nemo20435@gmail.com>
2023-12-21 11:03:26 +0900

    JSON Data 포맷으로 응답하기(@ResponseBody)

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
@@ -18,9 +19,19 @@ import kr.board.mapper.BoardMapper;
 @Controller
 public class BoardController {
 	
+	@Autowired
+	BoardMapper boardMapper;
+	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
+	}
+	
+	//@ResponseBody -> jackson-databind(객체를 ->JSON 데이터 포맷으로 변환)
+	@RequestMapping("/boardList.do")
+	public @ResponseBody List<Board> boardList() {
+		List<Board> list = boardMapper.getLists();
+		return list; //JSON 데이터 형식으로 변환(api)해서 리턴(응답)하겠다
 	}
 
 }
